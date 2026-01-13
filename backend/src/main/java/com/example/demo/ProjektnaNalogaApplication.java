506d0d4e60074ca3c1690963d23a23fe6c0cbd08
Tej Verbic SCV <tej.verbic@scv.si>
2026-01-13 09:53:33 +0100

    Setup backend datotek

---
+++ backend/src/main/java/com/example/demo/ProjektnaNalogaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektnaNalogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektnaNalogaApplication.class, args);
+	}
+
+}
